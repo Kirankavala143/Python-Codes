e6993187f768463d96dd02c0f1350b60f294d72d
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-21 17:56:23 +0530

    Adds prime check, divisor listing, and difference computation
    Implements functionality to check if a number is prime and lists all divisors of a given number.
    
    Also introduces a calculation for the difference between two multiplication tables.
    
    Commented out previous GCD and LCM code to simplify the file.

--- gfg.py
+++ gfg.py
@@ -24,14 +24,49 @@
 # print("Factorial of", n, "is", res)
 
 # find gcd of two numbers
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-small=min(a, b)
-for i in range(1,small+1):
-    if a%i==0 and b%i==0:
-        gcd=i
-print("GCD of", a, "and", b, "is", gcd)
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+# small=min(a, b)
+# for i in range(1,small+1):
+#     if a%i==0 and b%i==0:
+#         gcd=i
+# print("GCD of", a, "and", b, "is", gcd)
 
 # find lcm of two numbers
-lcm = (a * b) // gcd
-print("LCM of", a, "and", b, "is", lcm)
+# lcm = (a * b) // gcd
+# print("LCM of", a, "and", b, "is", lcm)
+
+# Check if a number is prime
+n = int(input("Enter a number: "))
+if n<=1:
+    print("Enter the number greater than 1")
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print(n, "is not a prime number")
+            break
+    else:
+        print(n, "is a prime number")
+
+# All divisors of a number
+n = int(input("Enter a number: "))
+for i in range(1,n):
+    if n % i == 0:
+        print(i)
+    
+# difference between two tables
+n1 = int(input())
+n2 = int(input())
+
+for i in range(1,11):
+    print(n1*i-n2*i,end=" ")
+
+n = 4
+for i in range(n):
+    print("* " * n)
+
+
+    
+
+
+
