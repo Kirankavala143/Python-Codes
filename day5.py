564a4378eb871fd46f206727a6ab6270960247b4
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-21 21:40:41 +0530

    Adds a new function for combining names
    Introduces a `name` function that concatenates first and last names.
    
    Removes an unused temporary file and cleans up the code to focus on relevant functionality.

---
+++ day5.py
@@ -0,0 +1,22 @@
+# # FUNCTIONS
+# def day(d,m,y):
+#     print(d,m,y,sep="-")
+
+# print("hi")
+
+# day("21","06","2025")
+# print("this is the today")
+
+# def day(d,m,y):
+#     return d +"-"+m+"-"+y
+
+# print("hi")
+
+# d=day("21","06","2025")
+# print(d)
+# print("this is the today")
+
+def name(first, last):
+    return first + " " + last
+
+print(name("John", "Doe"))  # Output: John Doe
