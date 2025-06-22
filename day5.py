7790881cf656740cbfb23e26173b5f6ade74514f
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-22 11:34:24 +0530

    Adds mathematical functions and first digit extractor
    Introduces an `add` function that calculates the sum and product of two numbers.
    
    Implements a `getfirstdigit` function to extract the first digit from a given number, enhancing user interaction by prompting for input.
    
    Updates the divisor loop to include the number itself for complete divisor listing.

--- day5.py
+++ day5.py
@@ -20,3 +20,20 @@ def name(first, last):
     return first + " " + last
 
 print(name("John", "Doe"))  # Output: John Doe
+
+def add(a, b):
+    sum=a+b
+    mul=a*b
+    return sum,mul
+s,m=add(5,6)
+print("Sum:", s)  # Output: Sum: 11
+print("Product:", m)  # Output: Product: 30
+
+
+def getfirstdigit(x):
+    while x>=10:
+        x=x//10
+    return x
+
+x=int(input("Enter a number: "))
+print("First digit:", getfirstdigit(x))  # Output: First digit: 1 (for example, if input is 1234)
--- gfg.py
+++ gfg.py
@@ -50,7 +50,7 @@ else:
 
 # All divisors of a number
 n = int(input("Enter a number: "))
-for i in range(1,n):
+for i in range(1,n+1):
     if n % i == 0:
         print(i)
     
