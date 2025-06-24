ad5abe6d688b380538ad128d1adf12390204d771
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-24 21:23:15 +0530

    Adds function to find the smallest missing positive number
    Implements a new function to efficiently find the smallest missing positive integer in a list.
    
    This addition enhances the functionality of the code by providing a solution for identifying missing values, which can be useful in various applications.
    
    Improves code usability and extends the current capabilities of the existing functions.

--- gfg.py
+++ gfg.py
@@ -136,9 +136,20 @@ def distinct(l):
             res=res+1
     return res  
     # return len(set(l)) 
-    
+
 l = [1, 2, 3, 4, 5, 1, 2]
 print("Distinct elements count:", distinct(l))  # Output: 5
 
+
+# smallest postive mising number
+def smallest_missing_positive(arr):
+    s = set(arr)  # Convert list to set for O(1) lookups
+    num = 1       # Start checking from 1
+    while True:
+        if num not in s:
+            return num  # Found the missing number!
+        num += 1        # Check next number
+print(smallest_missing_positive([3, 4, -1, 1]))  # Output: 2
+print(smallest_missing_positive([1, 2, 0]))      # Output:3
 
 
