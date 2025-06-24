2915f4590a8c5b4fdf6dfa9881a3b7d0ed0e23ae
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-24 23:12:16 +0530

    Refactors smallest missing positive number function
    Removes the old implementation of the smallest missing positive number function and replaces it with a new version for improved readability and efficiency.
    
    Updates the function to use a more concise naming and structure while maintaining its core logic.
    
    Additionally, modifies the test cases to validate the new function implementation.

--- gfg.py
+++ gfg.py
@@ -142,15 +142,23 @@ print("Distinct elements count:", distinct(l))  # Output: 5
 
 
 # smallest postive mising number
-def smallest_missing_positive(arr):
-    s = set(arr)  # Convert list to set for O(1) lookups
-    num = 1       # Start checking from 1
+# def smallest_missing_positive(arr):
+#     s = set(arr)  # Convert list to set for O(1) lookups
+#     num = 1       # Start checking from 1
+#     while True:
+#         if num not in s:
+#             return num  # Found the missing number!
+#         num += 1        # Check next number
+# print(smallest_missing_positive([3, 4, -1, 1]))  # Output: 2
+# print(smallest_missing_positive([1, 2, 0]))      # Output:3
+
+
+def small(arr):
+    s=set(arr)
+    num=1
     while True:
         if num not in s:
-            return num  # Found the missing number!
-        num += 1        # Check next number
-print(smallest_missing_positive([3, 4, -1, 1]))  # Output: 2
-print(smallest_missing_positive([1, 2, 0]))      # Output:3
-
-
-
+            return num
+        num+=1
+arr = [5, 4, -1, 1]
+print(small(arr))  # Output: 2
