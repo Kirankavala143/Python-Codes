aa94272ea09a8fef67428a36f833e418b9bd4200
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-27 18:41:40 +0530

    updated the code

--- dayy2.py
+++ dayy2.py
@@ -1,10 +1,10 @@
-a="kiran"
-print(a[::-1])
-print(''.join(reversed(a)))
-re=""
-for i in a:
-    re=i+re
-print(re)
+# a="kiran"
+# print(a[::-1])
+# print(''.join(reversed(a)))
+# re=""
+# for i in a:
+#     re=i+re
+# print(re)
 
 # Count vowels and consonants
 a="kirankumar"
@@ -29,3 +29,31 @@ for i in a:
 print(b)
 
 # Find the most frequent character
+a = "kirankumar"
+count = {}
+# Count character frequencies
+for i in a:
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+# Find the character with the highest frequency
+most_frequent_char = max(count, key=count.get)
+
+print("Most frequent character:", most_frequent_char)
+print("Frequency:", count[most_frequent_char])
+
+# Capitalize the first letter of each word
+a = "hello is od kirankk world"
+words = a.split()
+print(words)
+capitalized_words = [word.capitalize() for word in words]
+result = " ".join(capitalized_words)
+print(result)
+
+def fun(a,position,newchar):
+    return a[:position] + newchar + a[position+1:]
+string = "kirankumar"
+position = 5
+character = 's'
+print(fun(string,position,character))
