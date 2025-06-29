4f96a20ab5b5ca4acd286c75b738d991aec7b10c
Kirankavala143 <kavalakiran98@gmail.com>
2025-06-29 15:22:06 +0530

    Adds functions for even/odd check, max value, palindrome, factorial, and Fibonacci
    Implements multiple utility functions including checks for even/odd numbers, finding the maximum of three numbers, palindrome validation, calculating factorial using recursion, and retrieving the nth Fibonacci number.
    
    Enhances functionality with example usage for each function to facilitate user interaction and understanding.

--- dayy3.py
+++ dayy3.py
@@ -1,10 +1,55 @@
-# FUNCTIONS
-# Write a function to check if a number is even or odd.
-def even_odd(num):
-    if num % 2 == 0:
-        return "Even"
+# # FUNCTIONS
+# # Write a function to check if a number is even or odd.
+# def even_odd(num):
+#     if num % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+# # Example usage
+# s=int(input("enter a number: "))
+# print(even_odd(s))  # Output: Even
+
+# # Create a function that finds the maximum of three numbers.
+# def max(a, b, c):
+#     if a >= b and a >= c:
+#         return a
+#     elif b >= a and b >= c:
+#         return b
+#     else:
+#         return c
+# # Example usage
+# a=int(input("enter a number: "))
+# b=int(input("enter a number: "))
+# c=int(input("enter a number: "))
+# print(max(a,b,c))
+
+# Write a function to check if a string is a palindrome.
+def palindrome(s):
+    return s==s[::-1]
+s=input("enter a string:")
+if palindrome(s):
+    print("palindrome")
+else:
+    print("Not Palindrome")
+
+# Calculate factorial using recursion.
+def fact(n):
+    if n==0 or n==1:
+        return 1
     else:
-        return "Odd"
-# Example usage
-s=int(input("enter a number: "))
-print(even_odd(s))  # Output: Even
+        return n*fact(n-1)
+    
+n=int(input("enter a number: "))
+print(fact(n))  # Output: Factorial of n
+
+# Find the nth Fibonacci number using recursion.
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+n = int(input("Enter the position of Fibonacci number: "))
+print(f"The {n}th Fibonacci number is: {fibonacci(n)}")  #  
+# Output: Fibonacci number at position n
