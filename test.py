4931862f444bc5b95538621271e5d687f61c464a
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-01 15:45:48 +0530

    Refactors and consolidates multiple Python exercises
    Consolidates various Python exercises into a structured format, improving readability and organization.
    
    Removes commented-out Armstrong number code and replaces it with a more efficient approach for calculating notes required for a given amount.
    
    Introduces several new functions for different programming tasks, including printing a hollow pyramid, counting perfect squares, and showing odd/even numbers, enhancing the learning materials for users.

--- test.py
+++ test.py
@@ -54,37 +54,119 @@ if d > 0:
 tot = score + bonus
 print(tot)
 
-#  Armstrong number
-num=int(input("enter a number:"))
-original_num=num
-length=len(str(num))
-sum=0
-while num>0:
-    digit=num%10
-    sum+=digit**length
-    num=num//10
+# 3 Armstrong number
+# num=int(input("enter a number:"))
+# original_num=num
+# length=len(str(num))
+# sum=0
+# while num>0:
+#     digit=num%10
+#     sum+=digit**length
+#     num=num//10
 
-if sum==original_num:
-    print("armstrong number")
-else:
-    print("not an armstrong number")
+# if sum==original_num:
+#     print("armstrong number")
+# else:
+#     print("not an armstrong number")
 
 # Write a program that reads an amount A and prints the minimum number of 2000, 500, 200, 50, 
 # 20, 5, 2 and 1 rupee notes required for the given amount.
-A=int(input())
 
-print(A//2000)
-A=A%2000
-print(A//500)
-A=A%500
-print(A//200)
-A=A%200
-print(A//50)
-A=A%50
-print(A//20)
-A=A%20
-print(A//5)
-A=A%5
-print(A//2)
-A=A%2
-print(A//1)
+# 4
+
+# A=int(input())
+# notes=[2000,500,200,100,50,20,5,2,1]
+# for i in notes:
+#    count=A//i
+#    print(f"{i} :{count}",end=" ")
+#    A=A%i
+
+#5 Given a number N write a program to print a Hollow Pyramid of 2*N-1 rows using numbers 
+# Sample input1: 
+# 4 
+# Sample output1: 
+# 1 
+# 2 2 
+# 3   3 
+# 4     4 
+# 3   3 
+# 2 2 
+# 1
+
+# n = int(input("Enter N: "))
+
+# # Upper half of the pyramid
+# for i in range(1, n + 1):
+#     if i == 1:
+#         print(i)
+#     else:
+#         print(i, end=" ")
+#         print(" " * (2 * i - 3), end="")
+#         print(i)
+
+# # Lower half (inverted)
+# for i in range(n - 1, 0, -1):
+#     if i == 1:
+#         print(i)
+#     else:
+#         print(i, end=" ")
+#         print(" " * (2 * i - 3), end="")
+#         print(i)
+
+
+# 6 You are given two numbers A and B where 1<=A<=B write a program to find number of perfect 
+# squares in the range A and B (including A and B)
+
+# A=int(input())
+# B=int(input())
+# count=0
+# for i in range(A,B+1):
+#     if i**0.5==int(i**0.5):
+#         count+=1
+# print(count)
+
+# import math
+# A = int(input())
+# B = int(input())
+# count = math.floor(math.sqrt(B)) - math.ceil(math.sqrt(A)) + 1
+# print(count)
+
+# 7 odd,even
+
+# def show_numbers(num):
+#     for i in range(1,num+1):
+#       if i%2==0:
+#         print(i,"even")
+#       else:
+#         print(i,"odd")
+
+# num=int(input())
+# show_numbers(num)
+    
+#8 reverse 
+# s="kiran"
+# for i in s[::-1]:
+#     print(i)
+
+# order number
+# n=int(input())
+# l=[]
+# for i in range(1,n+1):
+#     print(i)
+#     l.append(i)
+#     print(l)
+
+#9 user input
+# n=int(input())
+# list=[]
+# for i in range(1,n+1):
+#     num=int(input())
+#     list.append(num)
+# print(list)
+
+# 10 add new key value
+Student_dict={ "Ram" : "Cricket",  "Naresh":"Football", "Vani":"Tennis", "Rahim":"Cricket"}
+keys,values=input().split()
+Student_dict[keys] = values
+print(Student_dict)
+  
