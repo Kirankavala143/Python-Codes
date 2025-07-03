2fb53c9be53bae9d02f9c30acfec255861555bbb
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-03 21:26:39 +0530

    Adds basic array operations and a while loop example
    Introduces a new script demonstrating basic array operations, including creating, reversing, checking for duplicates, and sorting arrays.
    
    Implements a while loop that prints numbers until the input number is greater than zero, correcting the loop condition for proper functionality.

---
+++ dayy6.py
@@ -0,0 +1,19 @@
+# Find time complexity of:
+
+# Creating an array
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a)
+
+
+# Reversing an array
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a[::-1])
+
+# Checking for a duplicate in a list
+a=[1,2,3,4,5,6,7,8,9,10]
+print(3 in a)
+
+# Sorting a list using built-in sort
+a=[1,2,7,5,9,4,3,9,10]
+a.sort()
+print(a)
---
+++ test1.py
@@ -0,0 +1,10 @@
+# while loop to print reverse of numbers
+n = int(input("Enter a number: "))
+# for i in range(n, 0, -1):
+#     print(i, end=" ")
+# while n>0:
+#     print(n)
+#     n-=1
+while n<=0:
+    print(n)
+    n += 1
