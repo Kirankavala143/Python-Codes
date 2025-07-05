c2ed3089be6e385197052e1c3df2dbf5f5eba87f
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-05 21:42:40 +0530

    Adds number classification functions to enhance functionality
    Introduces functions to check for perfect, automorphic, spy, and palindrome numbers, improving the program's utility for number classification.
    
    Updates the reverse number printing logic from a for loop to a while loop for better clarity.
    
    Enhances user interaction by allowing input for various number checks.

--- test1.py
+++ test1.py
@@ -50,3 +50,9 @@ else:
 # n = int(input("Enter a number: "))
 # for i in range(n, 0, -1):
 #     print(i, end=" ")
+
+# while loop to print reverse of numbers
+n = int(input("Enter a number: "))
+while n>0:    
+    print(n)      
+    n-=1              
--- test2.py
+++ test2.py
@@ -1,1 +1,70 @@
 # neon number
+
+# def is_neon_number(n):
+#     square = n * n
+#     digit_sum = sum(int(digit) for digit in str(square))
+#     return digit_sum == n
+
+# n = int(input("Enter a number: "))
+# if is_neon_number(n):
+#     print(f"{n} is a Neon number")
+# else:
+#     print(f"{n} is not a Neon number")
+
+#spy number
+# def is_spy_number(n):
+#     digit_sum = 0
+#     digit_product = 1
+#     while n > 0:
+#         digit = n % 10
+#         digit_sum += digit
+#         digit_product *= digit
+#         n //= 10
+#     return digit_sum == digit_product
+
+# n = int(input("Enter a number: "))
+# if is_spy_number(n):
+#     print(f"{n} is a Spy number")
+# else:
+#     print(f"{n} is not a Spy number")
+
+# automorphic number
+# def is_automorphic_number(n):
+#     square = n * n
+#     return str(square).endswith(str(n))     
+
+# n = int(input("Enter a number: "))
+# if is_automorphic_number(n):    
+#     print(f"{n} is an Automorphic number")
+# else:           
+#     print(f"{n} is not an Automorphic number")
+
+# def is_palindrome(n):
+#     original = n     
+#     reverse = 0     
+#     while n > 0:
+#         remainder = n % 10     
+#         reverse = reverse * 10 + remainder     
+#         n //= 10     
+#     return original == reverse        
+# n = int(input("Enter a number: "))
+# if is_palindrome(n):    
+#     print(f"{n} is a Palindrome number")  
+# else:
+#     print(f"{n} is not a Palindrome number")
+
+# perfect number
+def is_perfect_number(n):
+    if n < 1:
+        return False
+    divisors = []         
+    for i in range(1, n):
+        if n % i == 0:
+            divisors.append(i)             
+    return sum(divisors) == n
+
+n = int(input("Enter a number: "))
+if is_perfect_number(n):
+    print(f"{n} is a Perfect number")    
+else:
+    print(f"{n} is not a Perfect number")
