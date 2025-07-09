84bb39291185aa1ff323f7e3405c8d222cec6676
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-09 18:40:30 +0530

    Adds function documentation and examples in function.py
    Enhances the codebase by introducing comprehensive function documentation, including syntax, argument types, and examples for better understanding.
    
    Removes previous sum function implementation and replaces it with structured explanations of various function types and their usage, improving code clarity for future development.
    
    This update aims to facilitate learning and usage of functions in Python for new developers.

--- function.py
+++ function.py
@@ -1,9 +1,89 @@
-def sum(end):
-    sum=0
-    for i in range(1,end+1):
-        sum=sum+i
-    return sum 
-var=sum(10)
-var2=sum(20)
-print(var)
-print(var2)
+# def sum(end):
+#     sum=0
+#     for i in range(1,end+1):
+#         sum=sum+i
+#     return sum 
+# var=sum(10)
+# var2=sum(20)
+# print(var)
+# print(var2)
+
+
+#Syntax
+'''def function_name(parameters):
+        #block of code
+        return value  #(optional)'''
+
+'''Term        -        Description
+    def        -   keyword to define function
+function_name  -   Any valid python identifier
+  parameters   -   input passes to the function
+     :         -   Ends the function header
+indentend block-   code that runs when the fucntion is called
+   return      -   Sends result back to caller '''
+
+#Example:-
+def hi():
+    print("Hello students")
+hi()    # Hello students
+#Ex1:-
+def students():
+    return 80
+total = students()
+print("Total students :",total)  # Total students : 80
+#Ex2-without using function calling:-
+def stu():
+    return 80
+tot = stu()
+print("Total students :",80) # Total students : 80
+
+'''Function Arguments
+3types of arguments
+1.positional 2.Keyword 3.Default
+
+#ex-on positional'''
+def hi(name):
+    print("hello",name)
+hi("vijju")  # hello vijju
+
+'''#ex- on Default'''
+def hi(name="friends"):
+    print("Hello",name)
+hi()        # Hello friends
+hi("Akki")  # Hello Akki
+
+'''#ex- on Keyword'''
+def stu(name,age):
+    print("Name:",name)
+    print("Age:",age)
+stu(age = 19, name = "Akhila")  
+
+'''------------------------------------------------------------------
+   | Feature    |     Description               |    Example        |
+   ------------------------------------------------------------------             
+   |    def     |    defines a function        |   def hi()         |
+   |  Arguments |    pass data into a function |   def hi(name):    |
+   |   return   |    Sends result back         |   return a+b       |
+   |    call    |    Use the function          |   Hi("Vijju")      | 
+   ------------------------------------------------------------------ '''
+
+#Types of functions:-
+#examples:-
+'''1.No Arguments, No return value'''
+def fun():
+    print("Keep smiling")
+fun()   # Keep smiling
+'''2.With Arguments, No Return value'''
+def greet(name):
+    print("Hello",name)
+greet("students")  # Hello students
+'''3.No Arguments, With Return value'''
+def hi():
+    return "friends"
+value = hi()
+print("Wishes:",value) # Wishes: friends
+'''4.with Arguments and Return value'''
+def add(a,b):
+    return a+b
+res = add(2,5)
+print("Sum:",res)  #Su
