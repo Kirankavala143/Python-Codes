13f4a99052701636cf2e01cf4c8b03252f04afc8
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-10 13:18:17 +0530

    Adds a pyramid pattern printing feature
    Introduces a nested loop to print a pyramid pattern of asterisks.
    
    This enhancement improves the output capabilities of the script
    by visually demonstrating nested loops in Python.
    
    No related issues or tickets.

--- dayy2.py
+++ dayy2.py
@@ -84,3 +84,10 @@ print(f"Fibonacci sequence of length {n}: {fib_sequence}")
 # print(count_substring(string, sub_string))
 
 print(2 == 2.0)
+
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+        
