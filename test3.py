cfb5c5f251bbb2d35bdd6c01ea41a5cd6b6acc7c
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-11 17:41:24 +0530

    Adds functions to calculate sum of numbers and digits
    Introduces two methods for summing: one for a list of numbers input by the user and another for calculating the sum of digits of a single number using recursion.
    
    Enhances code clarity and usability for numerical operations.

---
+++ test3.py
@@ -0,0 +1,23 @@
+# # sum of numbers in a input
+# n = int(input("Enter the number of elements: "))
+# count=0
+# word=str(n)
+# for i in word:
+#     count+=int(i)
+# print("Sum of numbers is:", count)
+
+# using sum function
+# print("Sum of numbers is:", sum(map(int, input("Enter numbers separated by space: ").split())))
+
+
+# sum of digits using recursion
+# def sum_of_digits(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return (n % 10) + sum_of_digits(n // 10)
+
+# # Example usage
+# num = int(input("Enter a number: "))
+# print("Sum of digits:", sum_of_digits(num))
+
